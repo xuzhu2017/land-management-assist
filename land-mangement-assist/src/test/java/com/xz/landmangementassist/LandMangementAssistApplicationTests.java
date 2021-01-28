a2537f1409a56b75cda053327922e721e8128855
xuzhu2017 <a753090302@163.com>
2021-01-28 17:05:29 +0800

    first

---
+++ land-mangement-assist/src/test/java/com/xz/landmangementassist/LandMangementAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xz.landmangementassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandMangementAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
