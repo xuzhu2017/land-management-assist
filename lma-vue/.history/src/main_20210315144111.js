f81d684f42df698fc679413464f97b40fcfaf95c
xuzhu2017 <a753090302@163.com>
2021-03-15 16:59:48 +0800

    新vue项目运行

---
+++ lma-vue/.history/src/main_20210315144111.js
@@ -0,0 +1,118 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'echarts/theme/macarons.js'
+import store from '@/store'
+// 按需引入element-ui
+import {
+  Dialog,
+  Menu,
+  Submenu,
+  Input,
+  Checkbox,
+  Switch,
+  Select,
+  Option,
+  Button,
+  Table,
+  Tooltip,
+  Breadcrumb,
+  Form,
+  Tag,
+  Tree,
+  Alert,
+  Icon,
+  Row,
+  Col,
+  Progress,
+  Card,
+  Carousel,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Footer,
+  Timeline,
+  Link,
+  Divider,
+  Image,
+  Loading,
+  MessageBox,
+  Message,
+  Notification
+} from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(Pagination)
+Vue.use(Dialog)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Input)
+Vue.use(Checkbox)
+Vue.use(CheckboxButton)
+Vue.use(CheckboxGroup)
+Vue.use(Switch)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(Button)
+Vue.use(ButtonGroup)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Tooltip)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Tabs)
+Vue.use(TabPane)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Alert)
+Vue.use(Icon)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Upload)
+Vue.use(Progress)
+Vue.use(Spinner)
+Vue.use(Badge)
+Vue.use(Card)
+Vue.use(Rate)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(Carousel)
+Vue.use(CarouselItem)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Footer)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
+Vue.use(Link)
+Vue.use(Divider)
+Vue.use(Image)
+
+Vue.use(Loading.directive)
+
+Vue.prototype.$loading = Loading.service
+Vue.prototype.$msgbox = MessageBox
+Vue.prototype.$alert = MessageBox.alert
+Vue.prototype.$confirm = MessageBox.confirm
+Vue.prototype.$prompt = MessageBox.prompt
+Vue.prototype.$notify = Notification
+Vue.prototype.$message = Message
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
---
+++ lma-vue/.history/src/main_20210315150757.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'echarts/theme/macarons.js'
+import store from '@/store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
+
+var axios = require('axios')
+axios.defaults.baseURL = 'http://localhost:8443/api'
+// 使请求带上凭证信息
+axios.defaults.withCredentials = true
+
+Vue.prototype.$axios = axios
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
